5691b4c54732e4d6ce2602666844cb1b6d5e0ba7
olemeha <77707992+olemeha@users.noreply.github.com>
2022-02-08 12:24:39 -0600

    Update 02-functions.rkt

--- 02-functions.rkt
+++ 02-functions.rkt
@@ -3,6 +3,19 @@
 (require racket/trace) ; for dynamic function call traces
 (require 2htdp/image) ; for a bit of fun with pictures
 
+(define *car-wheel* (circle 10 "solid" "blue"))
+
+(define *car-body* (beside/align "bottom"
+                    (square 20 "solid" "black")
+                    (square 35 "solid" "black")
+                    (square 20 "solid" "black")))
+
+(define *car* (let ([wheels (beside *car-wheel*
+                                   (rectangle 20 0 "solid" "white")
+                                   *car-wheel*)])
+                (overlay/offset wheels
+                                0 -15
+                                *car-body*)))
 #|-----------------------------------------------------------------------------
 ;; Function definitions
 
@@ -10,6 +23,12 @@
 - `define` supports a special syntax for binding variables to functions
 -----------------------------------------------------------------------------|#
 
+(define foo(lambda (x y) (+ x y)))
+
+(define (bar x y)
+  (* x y)
+  (/ x y)
+  (+ x y))
 
 
 #|-----------------------------------------------------------------------------
@@ -40,13 +59,49 @@
 - `equal?` for value-based equality
 -----------------------------------------------------------------------------|#
 
-
+(define lst '(a b c))
 
 #|-----------------------------------------------------------------------------
 ;; Recursion
 -----------------------------------------------------------------------------|#
 
+;0 1 1 2 3 5 8 13 21
+(define (fib n)
+  (if (< n 2)
+      n
+      (+ (fib (- n 1))(fib (- n 2)))))
+(trace fib)
 
+(define (sum-to n)
+  (if (= n 0)
+      0
+      (+ n (sum-to (- n 1)))))
+(trace sum-to)
+
+(define (sum-to-2 n s)
+  (if (= n 0)
+      s
+      (sum-to-2 (- n 1) (+ n s))))
+(trace sum-to-2)
+
+(define (sum-to-acc n sum)
+  (if (= n 0)
+      sum
+     (sum-to-acc (- n 1) (+ sum n))))
+(trace sum-to-acc)
+
+
+(define(sum-to-acc-2 n)
+  (let rec ([i n]
+            [acc 0])
+    (if (= i 0)
+        acc
+        (rec (- i 1)(+ acc i)))))
+     
+(define (loop n sexp)
+  (when (> n 0)
+    (eval sexp)
+    (loop (- n 1) sexp)))
 
 #|-----------------------------------------------------------------------------
 ;; On lists
@@ -54,8 +109,30 @@
 - recursion over lists is an example of "structural recursion"
 -----------------------------------------------------------------------------|#
 
+(define (length lst)
+  (if (empty? lst)
+      0
+      (+ 1 (length (rest lst)))))
+(trace length)
 
+(define (repeat n x)
+  (if (= n 0)
+      '()
+      (cons x (repeat (- n 1) x))))
+(trace repeat)
+(define (reverse lst)
+  (trace-let rec ([lst lst]
+            [acc '()])
+  (if (empty? lst)
+      acc
+      (rec (rest lst)(cons (first lst) acc)))))
 
+(define (range n)
+  (let rec ([i 0]
+            [acc '()])
+    (if (= i n)
+        (reverse acc)
+        (rec (+ i 1) (cons i acc)))))
 #|-----------------------------------------------------------------------------
 ;; Higher-order functions (HOFs)
 
